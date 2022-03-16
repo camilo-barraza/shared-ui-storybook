8365e8dff1a3a0aa776af8c50f4ba4fde833d21c
Camilo Barraza <cbarraza11personal@gmail.com>
2022-03-16 00:55:05 -0500

    add project files

---
+++ .storybook/preview.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { theme } from "../src/shared-ui/styledConfig";
+import { ThemeProvider } from "styled-components";
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
